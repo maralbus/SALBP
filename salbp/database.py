99efb34f59b772cc1976fdacbc145497a008a027
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2020-06-23 15:40:18 +0200

    [CODE] preprocessing of operations

--- salbp/database.py
+++ salbp/database.py
@@ -1,5 +1,6 @@
-#%%
+# %%
 import pandas as pd
+import numpy as np
 import operator
 import functools
 
@@ -12,8 +13,8 @@ class Database:
                             'providesSeparationOperation']
 
     def _read_database(self):
-        columns = ['isProcessTypeMD', 'hasName', 'hasPrice', 'hasFootprint', 'providesOperation']
-        df = pd.read_excel( "../data/database/Denso2021_SP1_ResourceDatabase_v0.18.xlsx", sheet_name='Resources', header=1, usecols=columns)
+        columns = ['isProcessTypeMD', 'hasName', 'hasPrice', 'hasFootprint', 'providesOperation', 'hasCycleTime']
+        df = pd.read_excel(self.filepath, sheet_name='Resources', header=1, usecols=columns)
         df = df.drop([0, 1], axis=0).reset_index(drop=True)
         df.hasFootprint[df.hasFootprint.isnull()] = '0x0'
         df.hasFootprint = df.hasFootprint.apply(lambda x: self._product_footprint(x))
@@ -37,7 +38,7 @@ class Database:
         check if the data provides the operation for a given operation_name
 
         Args:
-            data (str): the data which is checked if it provides the ooperation
+            data (str): the data which is checked if it provides the operation
             operation_name (str): name of the operation
 
         Returns:
@@ -50,21 +51,40 @@ class Database:
         else:
             return 0
 
+    def _calc_cycle_time(self, data) -> float:
+        data = 0
+        return np.random.uniform(low=4.0, high=30.0)
+
 #%%
 if __name__ == "__main__":
     d = Database("../data/database/Denso2021_SP1_ResourceDatabase_v0.18.xlsx")
     df = d.df
     print(df)
+
 #%% 
     # prod = lambda s: functools.reduce(operator.mul, map(int, s.split('x')))
     # df.loc[~df.providesOperation.isnull(), 'providesOperation'].apply(lambda x: d._check_operation(x, 'providesHandlingOperationMove'))
     # df.loc[:, 'providesOperation'].drop_duplicates()
     for operation in d.operations:
         df[operation] = df.loc[:, 'providesOperation'].apply(lambda x: d._check_operation(x, operation))
+
 # %%
-    df.loc[91, :]
+df.loc[91, :]
+
 # %%
-    df.loc[91, 'providesOperation'].split(';')
+# 6AxisRobot "VP-5423" in Database pos: 341
+df.loc[91, 'providesOperation'].split(';')
 
+# %%
+df['hasCycleTime'] = df['hasCycleTime'].apply(lambda x: np.random.uniform(low=4.0, high=30.0))
 
+# %%
+from plotly import graph_objs as go
+# %%
 
+data = go.Scatter(x=df.index, y=df.hasCycleTime)
+fig = go.Figure(data=data)
+fig.show()
+
+# %%
+df[df['hasName'] == "VP-5423"]
